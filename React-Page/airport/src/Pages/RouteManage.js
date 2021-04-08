8227f351001cbc56176834c4818ad627a698adad
Braslyn Rodríguez Ramírez <braslynrrr999@gmail.com>
2021-04-08 01:15:17 -0600

    pagina de routes

--- React-Page/airport/src/Pages/RouteManage.js
+++ React-Page/airport/src/Pages/RouteManage.js
@@ -4,14 +4,57 @@ import {Button, Card} from "react-bootstrap";
 import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
 import swal from "sweetalert";
 import CustomSelect from "../Components/CustomSelect";
+import ReactDOM from "react-dom";
+const client = new WebSocket("ws://localhost:8089/server/schedule");
+
+client.onopen = function (event){
+    setTimeout( ()=> client.send("{Action:'get_all_route'}"),100)
+}
+
+client.onerror = function (event) {
+    onError(event)
+};
+
+function onError(event){
+    swal("Connection Error:"+event.data);
+}
 
 
+client.onmessage = function (event) {
+    let message = JSON.parse(event.data);
+    if (message !== null && message !== undefined) {
+        if (message.none === "none") {
+            swal("Error", "Something fails", "error");
+        } else {
+            if (message.action === "update") {
+                setTimeout(() => client.send("{Action:'get_all_route'}"), 100)
+            } else if (message !== null) {
+                sessionStorage.setItem("routes", event.data);
+                if (document.getElementById("update RouteTable") !== null) {
+                    ReactDOM.unmountComponentAtNode(document.getElementById("update RouteTable"));
+                    ReactDOM.render(renderRouteTable(selectRow, options), document.getElementById("update RouteTable"))
+                }
+            }
+        }
+
+    }
+
+}
+
+function parseRoute(data){
+    data.forEach(x =>{x.price="$"+x.price;x.origenid=parseCountries(y=>y.id===x.origenid);x.destinoid=parseCountries(y=>y.id===x.destinoid);})
+    return data;
+}
+function parseCountries(lambda){
+    let x=JSON.parse(sessionStorage.countries).find(lambda)
+    return x.name;
+}
 function renderRouteTable(selectRow,options){
-    let data=[{id:"1",origen:"CR",destination:"MX",duration:"3",price:"600",discount:"3"}]
+    let data= parseRoute(JSON.parse(sessionStorage.routes))
     return( <BootstrapTable data={data} hover={true}  pagination={ true } options={ options } selectRow={ selectRow }>
         <TableHeaderColumn dataField="id" isKey>ID</TableHeaderColumn>
-        <TableHeaderColumn dataField="origen" filter={ { type: 'TextFilter', delay: 500 }}>Origen</TableHeaderColumn>
-        <TableHeaderColumn dataField="destination" filter={ { type: 'TextFilter', delay: 500 }}>Destination</TableHeaderColumn>
+        <TableHeaderColumn dataField="origenid" filter={ { type: 'TextFilter', delay: 500 }}>Origen</TableHeaderColumn>
+        <TableHeaderColumn dataField="destinoid" filter={ { type: 'TextFilter', delay: 500 }}>Destination</TableHeaderColumn>
         <TableHeaderColumn dataField="duration">Duration</TableHeaderColumn>
         <TableHeaderColumn dataField="price" >Price</TableHeaderColumn>
         <TableHeaderColumn dataField="discount" >Discount</TableHeaderColumn>
@@ -57,11 +100,62 @@ const selectRow = {
 };
 
 function handleRowSelect(row) {
-    //swal("Select Something",`What do you want to do whith type planeManage #${row.id}?`,{buttons:["Update","Delete"]}).then(resp=>manageResp(resp,row));
+    swal("Select Something",`What do you want to do whith route #${row.id}?`,{buttons:["Delete","Update"]}).then(resp=>manageRouteResp(resp,row));
 }
+
+function manageRouteResp(resp,row){
+    if(resp) {//update
+        document.getElementById("routeid").style.display="block";
+        let countries=JSON.parse(sessionStorage.countries);
+        document.getElementById("routeid").readOnly=true;
+        document.getElementById("routeid").innerText=row.id;
+        document.getElementById("duration").value=row.duration;
+        document.getElementById("price").value=row.price;
+        document.getElementById("discount").value=row.discount;
+        document.getElementById("originR").value=countries.find(x=>x.name===row.origenid).id;
+        document.getElementById("destinationR").value=countries.find(x=>x.name===row.destinoid).id;
+        document.getElementById("actionRoute").innerText="Update";
+    }else{//delete
+
+    }
+
+}
+
 function parceCountries(countries){
     let out=countries.map(ele=>({value:ele.id,text:ele.name}));
     return out;
+}
+
+
+function refresh(){
+    let countries=JSON.parse(sessionStorage.countries);
+    document.getElementById("routeid").style.display="none";
+    document.getElementById("routeid").innerText="";
+    document.getElementById("duration").value="";
+    document.getElementById("price").value="";
+    document.getElementById("discount").value="";
+    document.getElementById("actionRoute").innerText="ADD";
+    document.getElementById("originR").value=countries[0].id;
+    document.getElementById("destinationR").value=countries[0].id;
+}
+
+function excecuteRoute(){
+    let price=document.getElementById("price").value;
+    let message= {
+        id:document.getElementById("routeid").innerText,
+        duration:document.getElementById("duration").value,
+        price:price[0]==="$"? price.slice(1):price,
+        discount:document.getElementById("discount").value,
+        origenid:document.getElementById("originR").value,
+        destinoid:document.getElementById("destinationR").value
+    }
+    if(document.getElementById("actionRoute").innerText==="ADD"){
+        message.Action="create_route";
+        setTimeout( ()=> client.send(JSON.stringify(message)) , 100)
+    }else{
+        message.Action="update_route";
+        setTimeout( ()=> client.send(JSON.stringify(message)) , 100)
+    }
 }
 
 class RouteManage extends Component{
@@ -71,32 +165,32 @@ class RouteManage extends Component{
             <Card className="mx-auto" style={{width: '46rem'}}>
                 <Card.Body>
                     <Card.Title className="text-center">Route List</Card.Title>
-                    <div className="update RouteTable">
-                        {renderRouteTable(selectRow,options)}
+                    <div id="update RouteTable">
+
                     </div>
-                    <Card.Title className="text-center">Update Route<Button variant="success">Refresh</Button></Card.Title>
+                    <Card.Title className="text-center">Update Route<Button onClick={refresh} variant="success">Refresh</Button></Card.Title>
                     <div className="row mx-auto">
                         <div className="col">
-                            <label>ID: <input type="text"/></label>
+                            ID:<label id="routeid"></label>
                         </div>
                         <div className="col">
-                            <label>Origin from: <CustomSelect id="originE" data={parceCountries(JSON.parse(sessionStorage.countries))} /></label>
+                            <label>Origin from: <CustomSelect id="originR" data={parceCountries(JSON.parse(sessionStorage.countries))} /></label>
                         </div>
                         <div className="col">
-                            <label>Destination to: <CustomSelect id="destinationE" data={parceCountries(JSON.parse(sessionStorage.countries))} /></label>
+                            <label>Destination to: <CustomSelect id="destinationR" data={parceCountries(JSON.parse(sessionStorage.countries))} /></label>
                         </div>
                     </div>
                     <div  className="row">
                         <div className="col">
-                            <label>Duration  : <input /></label>
+                            <label>Duration: <input id="duration"/></label>
                         </div>
                         <div className="col">
-                            <label>Price  : <input /></label>
+                            <label>Price: <input id="price"/></label>
                         </div>
                         <div className="col">
-                            <label>Discount: <input /></label>
+                            <label>Discount: <input id="discount"/></label>
                         </div>
-                        <div className="row mx-auto"><Button variant="outline-primary">Update</Button></div>
+                        <div className="row mx-auto"><Button onClick={excecuteRoute} id="actionRoute" variant="outline-primary">ADD</Button></div>
                     </div>
                 </Card.Body>
             </Card>
--- React-Page/airport/src/Pages/ScheduleManage.js
+++ React-Page/airport/src/Pages/ScheduleManage.js
@@ -1,6 +1,100 @@
 import React,{Component} from "react";
 import Bar from "../Components/adminBar";
 import {Card} from "react-bootstrap";
+import swal from "sweetalert";
+import ReactDOM from "react-dom";
+import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
+const client = new WebSocket("ws://localhost:8089/server/schedule");
+const Week=[{day:1,name:"Monday"},{day:2,name:"Tuesday"},{day:3,name:"Wednesday"},
+    {day:4,name:"Thursday"},{day:5,name:"Friday"},{day:6,name:"Saturday"},{day:7,name:"Sunday"}]
+
+
+
+client.onopen = function (event){
+    setTimeout( ()=> client.send("{Action:'get_all_schedule'}"),100)
+}
+
+client.onerror = function (event) {
+    onError(event)
+};
+
+function onError(event){
+    swal("Connection Error:"+event.data);
+}
+
+function renderShowsTotal(start, to, total) {
+    return (
+        <p style={ { color: 'blue' } }>
+            From { start } to { to }, totals is { total }
+        </p>
+    );
+}
+
+
+const options = {
+    page: 0,  // which page you want to show as default
+    sizePerPageList: [ {
+        text: '5', value: 5
+    }, {
+        text: '10', value: 10
+    } ], // you can change the dropdown list for size per page
+    sizePerPage: 5,  // which size per page you want to locate as default
+    pageStartIndex: 0, // where to start counting the pages
+    paginationSize: 2,  // the pagination bar size.
+    prePage: 'Prev', // Previous page button text
+    nextPage: 'Next', // Next page button text
+    firstPage: 'First', // First page button text
+    lastPage: 'Last', // Last page button text
+    paginationShowsTotal: renderShowsTotal,  // Accept bool or function
+    paginationPosition: 'bottom' ,// default is bottom, top and both is all available
+    // hideSizePerPage: true > You can hide the dropdown for sizePerPage
+    // alwaysShowAllBtns: true // Always show next and previous button
+    // withFirstAndLast: false > Hide the going to First and Last page button
+    //other options
+};
+
+function handleRowSelect(row) {
+    //swal("Select Something",`What do you want to do whith type planeManage #${row.id}?`,{buttons:["Update","Delete"]}).then(resp=>manageResp(resp,row));
+}
+
+const selectRow = {
+    mode: 'radio',
+    onSelect: handleRowSelect
+};
+
+function renderScheduleTable(selectRow,options){
+    let data=JSON.parse(sessionStorage.schedules);
+    data.forEach(x=>x.stime=Week.find(y=>y.day===x.stime).name);
+    return( <BootstrapTable data={data} hover={true}  pagination={ true } options={ options } selectRow={ selectRow }>
+        <TableHeaderColumn dataField="id" isKey>ID</TableHeaderColumn>
+        <TableHeaderColumn dataField="routeid" filter={ { type: 'TextFilter', delay: 500 }}>Route ID</TableHeaderColumn>
+        <TableHeaderColumn dataField="sdate">Start day</TableHeaderColumn>
+        <TableHeaderColumn dataField="stime" >Day</TableHeaderColumn>
+    </BootstrapTable>);
+
+}
+
+client.onmessage = function (event) {
+    let message = JSON.parse(event.data);
+    if (message !== null && message !== undefined) {
+
+        if (message.none === "none") {
+            swal("Error", "Something fails", "error");
+        } else {
+            if (message.action === "update") {
+                setTimeout(() => client.send("{Action:'get_all_schedule'}"), 100)
+            } else if (message !== null) {
+                sessionStorage.setItem("schedules", event.data);
+                if (document.getElementById("update ScheduleTable") !== null) {
+                    ReactDOM.unmountComponentAtNode(document.getElementById("update ScheduleTable"));
+                    ReactDOM.render(renderScheduleTable(selectRow, options), document.getElementById("update ScheduleTable"))
+                }
+            }
+        }
+
+    }
+
+}
 
 class ScheduleManage extends Component{
 
@@ -9,10 +103,8 @@ class ScheduleManage extends Component{
             <Bar></Bar>
             <Card className="mx-auto" style={{width: '46rem'}}>
                 <Card.Body>
-                    <Card.Title className="text-center">Add Schedule</Card.Title>
                     <Card.Title className="text-center">Schedules List</Card.Title>
-                    <div className="update ScheduleTable">
-
+                    <div id="update ScheduleTable">
                     </div>
                 </Card.Body>
             </Card>
